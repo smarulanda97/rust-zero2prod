6e4a493c52b167d0933a7902d189b80171f4f0ea
Santiago Marulanda <santiagomm1997@gmail.com>
2024-04-05 08:43:18 +0200

    Connect to the database in Digital Ocean

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,11 +1,9 @@
-use config::{Config, File};
-use secrecy::{ExposeSecret, Secret};
 use std::env;
+use sqlx::postgres;
+use config::{self, Config, File};
+use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
-/// # Environment
-///
-/// This enum holds the possible environment values for this
-/// application (local or production).
 pub enum Environment {
     Local,
     Production,
@@ -22,6 +20,7 @@ impl Environment {
 
 impl TryFrom<String> for Environment {
     type Error = String;
+
     fn try_from(value: String) -> Result<Self, Self::Error> {
         match value.to_lowercase().as_str() {
             "local" => Ok(Self::Local),
@@ -35,47 +34,47 @@ impl TryFrom<String> for Environment {
     }
 }
 
-/// # Settings
-///
-/// Global object that holds the whole configuration for the
-/// application and other services.
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
 }
 
-/// ## DatabaseSettings
-///
-/// Object that holds the properties with connection
-/// information for the database.
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub user: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub name: String,
+    pub require_ssl: bool
 }
 
 impl DatabaseSettings {
-    pub fn get_conn_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.user,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.name
-        ))
+    pub fn without_db(&self) -> postgres::PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            postgres::PgSslMode::Require
+        } else {
+            postgres::PgSslMode::Prefer
+        };
+
+        postgres::PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.user)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+
+    pub fn with_db(&self) -> postgres::PgConnectOptions {
+        self.without_db().database(&self.name)
     }
 }
 
-/// ## ApplicationSettings
-/// Object that holds the configuration of the application
-///
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -94,6 +93,11 @@ pub fn get_config() -> Result<Settings, config::ConfigError> {
     let settings = Config::builder()
         .add_source(File::from(config_directory.join("base.yaml")))
         .add_source(File::from(config_directory.join(environment_filename)))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
 
     settings.try_deserialize::<Settings>()
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
 use std::{io, net::TcpListener};
+use sqlx::postgres;
 use zero2prod::{configuration, startup, telemetry};
 
 #[tokio::main]
@@ -9,12 +8,12 @@ async fn main() -> io::Result<()> {
     telemetry::init_subscriber(subscriber);
 
     let config = configuration::get_config().expect("Failed to read the config file");
-    let db_pool = PgPool::connect_lazy(&config.database.get_conn_string().expose_secret())
-        .expect("Failed to connect to Postgres");
+    let connection_pool = postgres::PgPoolOptions::new()
+        .connect_lazy_with(config.database.with_db());
 
     // Starting a new listener for the webserver
     let app_address = format!("{}:{}", config.application.host, config.application.port);
     let app_listener = TcpListener::bind(app_address).expect("Failed to bind address");
 
-    startup::run(app_listener, db_pool)?.await
+    startup::run(app_listener, connection_pool)?.await
 }
